aaf3e54ce86b5c84d5ed79e590142ea647c24aac
Myles-Glenn Gaerlan <m.gaerlan@live.com>
2018-07-17 23:30:22 +1000

    Added Data Model for ComicBook & Artist

---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNo { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNo;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNo + ".jpg";
+            }
+        }
+    }
+}
